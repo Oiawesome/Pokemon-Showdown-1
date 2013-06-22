e946f902a2aa18a48762155a5663e26a07c849d0
Nollan101 <kgb@kgbudge.com>
2013-06-22 17:39:29 -0500

    Update rooms.js

--- rooms.js
+++ rooms.js
@@ -1029,7 +1029,6 @@ var BattleRoom = (function() {
 
 var ChatRoom = (function() {
 	function ChatRoom(roomid, title) {
-		var bannedChatWords = {fuck};
 		this.id = roomid;
 		this.title = title||roomid;
 		this.i = {};
@@ -1292,9 +1291,6 @@ var ChatRoom = (function() {
 	};
 	ChatRoom.prototype.chat = function(user, message, connection) {
 		message = CommandParser.parse(message, this, user, connection);
-		if (message in bannedChatWords) {
-			return false;	
-		}
 		if (message) {
 			this.add('|c|'+user.getIdentity(this.id)+'|'+message, true);
 		}
