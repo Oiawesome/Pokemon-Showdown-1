15054240c64c56881d018fe8477499a776a0fdf0
Nollan101 <kgb@kgbudge.com>
2013-07-01 09:23:14 -0600

    Update rooms.js

--- rooms.js
+++ rooms.js
@@ -1348,6 +1348,8 @@ console.log("NEW GLOBAL: global");
 rooms.global = new GlobalRoom('global');
 console.log("NEW CHATROOM: lobby");
 rooms.lobby = new ChatRoom('lobby', 'Lobby');
+console.log("NEW CHATROOM: authchat");
+rooms.authchat = new ChatRoom('authchat', 'Nollan\'s Factory Staff');
 
 exports.GlobalRoom = GlobalRoom;
 exports.BattleRoom = BattleRoom;
