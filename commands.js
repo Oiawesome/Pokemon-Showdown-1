fe2fb5bd7377889fabcd21d5be829e520b48b376
Nollan101 <kgb@kgbudge.com>
2013-06-30 18:32:34 -0600

    Update commands.js

--- commands.js
+++ commands.js
@@ -16,6 +16,7 @@ if (typeof balance == "undefined") {
 	balance = new Object();
 }
 function importUserBalance(user, balance) {
+	balance = parseInt(balance);
 	fs.readFile('config/userbalance.csv', function(err, data) {
 		if (err) {
 			console.log("The file was not properly read into the system! The error was: " + err);
